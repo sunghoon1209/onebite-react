11280436d19363db72dba931ac5498f94a9974b4
주성훈 <83044@naver.com>
2025-01-12 16:47:44 +0900

    chapter05추가가

--- section01/chapter04.js
+++ section01/chapter04.js
@@ -12,3 +12,8 @@ const birth = "1997.01.07"
 // 3-2. 숫자로 시작할 수 없다.
 
 // 3-3. 예약어를 사용할 수 없다.
+
+// 4. 변수 명명 가이드
+let salesCount = 1;
+let refundCount = 1;
+let totalSalesCount = salesCount - refundCount;
